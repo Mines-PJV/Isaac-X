02629d6bda150d9a28782dca68c70e896b59a28c
Julien Blatecky <julien1619@gmail.com>
2013-01-10 15:27:06 +0100

    Added backup script
    Signed-off-by: Julien Blatecky <julien1619@gmail.com>

---
+++ system/install.bat
@@ -0,0 +1,6 @@
+@echo on
+set PWD=%~dp0
+copy %PWD%..\..\..\..\UDKGame\Config\DefaultEngine.ini %PWD%backup\
+copy %PWD%..\..\..\..\UDKGame\Config\DefaultGame.ini %PWD%backup\
+copy %PWD%DefaultEngine.ini %PWD%..\..\..\..\UDKGame\Config\DefaultEngine.ini /Y
+copy %PWD%DefaultGame.ini %PWD%..\..\..\..\UDKGame\Config\DefaultGame.ini /Y
